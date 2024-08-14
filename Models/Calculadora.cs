1a29b842f9a85606c470ed8516b29fa4cefcc82b
Ikey Salgado <ikeysfr@gmail.com>
2024-08-14 14:57:44 -0300

    Aula operadores aritiméticos em C#

---
+++ Models/Calculadora.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Introdução_Operadores_Aritimeticos
+{
+    public class Calculadora
+    {
+        public void Somar(int x, int y)
+        {
+            Console.WriteLine($"{x} + {y} = {x + y}");
+        }
+
+        public void Subtrair(int x, int y)
+        {
+            Console.WriteLine($"{x} - {y} = {x - y}");
+        }
+
+        public void Multiplicar(int x, int y)
+        {
+            Console.WriteLine($"{x} * {y} = {x * y}");
+        }
+
+        public void Dividir(int x, int y)
+        {
+            Console.WriteLine($"{x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x} ^ {y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {Math.Round(seno, 4)}");
+        }
+
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {Math.Round(coseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Seno de {angulo}° = {Math.Round(tangente, 4)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Introdução_Operadores_Aritimeticos;
+
+Calculadora calc = new Calculadora();
+
+//calc.RaizQuadrada(9);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+//calc.Potencia(3, 3);
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 30);
+// calc.Multiplicar(10, 30);
+// calc.Dividir(10, 30);
+
+
+// int numeroIncremento = 10;
+
+// Console.WriteLine("Incrementando o 10");
+// //numero = numero + 1;
+// numeroIncremento++;
+
+// Console.WriteLine(numeroIncremento);
+
+
+// int numeroDecremento = 20;
+// Console.WriteLine("Decrementando o 20");
+// //numero = numero + 1;
+// numeroDecremento--;
+
+// Console.WriteLine(numeroDecremento);
